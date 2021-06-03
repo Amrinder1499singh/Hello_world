36bb8e10d915d66153bd8bdf105ecb937ddbcd63
Amrinder Singh <43899719+Amrinder1499singh@users.noreply.github.com>
2021-06-04 02:59:56 +0530

    Add files via upload

---
+++ demo.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include <math.h>
+int functionName(); // declaration
+int main(){
+    // How to define function in the c language
+    functionName(); //calling of function
+    return 0;
+
+
+}
+functionName(){
+    //function body
+    // print the table in c 
+    int num;
+    printf("Enter the number: ");
+    scanf("%d",&num);
+    for (int i = 1; i < num; i++)
+    {
+        /* code */
+        int tableTotal = i*num;
+        printf("%d x %d = %d",i,num,tableTotal);
+        return tableTotal;
+    }
+    
+
+}
